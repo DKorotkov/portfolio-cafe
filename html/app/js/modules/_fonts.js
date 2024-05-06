7a909af3611ef0230023a7701e1f732f45979d5a
Danila Korotkov <danila.korotkov@gmail.com>
2024-05-06 15:44:33 +0300

    add navigation

--- html/app/js/modules/_fonts.js
+++ html/app/js/modules/_fonts.js
@@ -1,32 +1,21 @@
-//----------------------------ШРИФТЫ------------------------------------------------------------
-var mainFont = new FontFaceObserver("Fira Sans");
-// var Raleway = new FontFaceObserver("Raleway");
+var FontFaceObserver = require('fontfaceobserver')
+export function loadFont() {
+	var mainFont = new FontFaceObserver('Poppins')
+	var Playfair_Display = new FontFaceObserver('Playfair Display')
 
-// // Загружаем все шрифты сразу Здесь лучше загружать шрифты, которые не являются основными, но находяться в верхней части страницы.
-// Promise.all([Montserrat.load()]).then(function () {
-//    console.log("Montserrat загружен");
-// });
+	var html = document.documentElement
 
-// Добавлем необходимые классы при загрузке нужного шрифта
-var html = document.documentElement;
-
-if (html.classList.contains("fonts-loading")) {
-   mainFont
-      .load()
-      .then(function () {
-         html.classList.remove("fonts-loading");
-         html.classList.add("fonts-loaded");
-         sessionStorage.fontsLoaded = true; // Загружаем в кэш
-      })
-      .catch(function () {
-         html.classList.remove("fonts-loading");
-         html.classList.add("fonts-failed");
-         sessionStorage.fontsLoaded = false;
-      });
+	if (html.classList.contains('fonts-loading')) {
+		Promise.all([mainFont.load(), Playfair_Display.load()])
+			.then(function () {
+				html.classList.remove('fonts-loading')
+				html.classList.add('fonts-loaded')
+				sessionStorage.fontsLoaded = true // Загружаем в кэш
+			})
+			.catch(function () {
+				html.classList.remove('fonts-loading')
+				html.classList.add('fonts-failed')
+				sessionStorage.fontsLoaded = false
+			})
+	}
 }
-
-// Здесь лучше загружать шрифты которые не основные и находяться в нижней части страницы
-// Montserrat.load().then(function () {
-//    console.log("Загружен Input Mono");
-// });
-// ________________________________________________________________________________________________________________
